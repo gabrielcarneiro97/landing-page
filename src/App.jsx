e855a35fdc36217a2b752658725c085a5c8df114
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-05-15 13:43:14 -0300

    Setup inicial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'antd/dist/antd.css';
+import React from 'react';
+import './App.css';
+import Forms from './Form';
+
+function App() {
+  return (
+    <Forms />
+  );
+}
+
+export default App;
